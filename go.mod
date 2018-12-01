854707916f1ef8c4559132852077b43e2e8bfa28
胡春亮 <huchunliang@gaoxinzb.com>
2018-12-01 17:16:51 +0800

    格式化代码

--- go.mod
+++ go.mod
@@ -9,3 +9,11 @@ require (
 	github.com/lib/pq v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16
 )
+
+replace (
+	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 => github.com/golang/crypto v0.0.0-20181030102418-4d3f4d9ffa16
+	golang.org/x/net v0.0.0-20180821023952-922f4815f713 => github.com/golang/net v0.0.0-20180826012351-8a410e7b638d
+	golang.org/x/sys v1.0.0 => /Users/huchunliang/Documents/workspace/src/golang.org/x/sys
+	golang.org/x/text v0.3.0 => github.com/golang/text v0.3.0
+	gopkg.in/yaml.v2 v2.0.0 => /Users/huchunliang/Documents/workspace/src/gopkg.in/yaml.v2
+)
--- utils/util.go
+++ utils/util.go
@@ -2,14 +2,18 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-func Message(status bool, message string) (map[string]interface{}) {
-	return map[string]interface{} {"status" : status, "message" : message}
+func Message(status bool, message string) map[string]interface{} {
+	return map[string]interface{}{"status": status, "message": message}
 }
 
-func Respond(w http.ResponseWriter, data map[string] interface{})  {
+func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	e := json.NewEncoder(w).Encode(data)
+	if e != nil {
+		log.Fatalf("解码异常,%d", e)
+	}
 }
